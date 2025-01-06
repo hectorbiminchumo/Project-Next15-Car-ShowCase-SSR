9196bd1e725909f0bfc87349ce09483b181516b7
Hector Biminchumo <hectorbiminchumo@gmail.com>
2025-01-06 09:56:34 -0500

    feat: filter data using searchParams in customFilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,15 +4,26 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
-import { Hedvig_Letters_Sans } from 'next/font/google';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0])
+
+  const handleUpdateParams = (e: {title: string, value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());  
+
+    router.push(newPathName)
+  }
+
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) =>{ 
+          setSelected(e)
+          handleUpdateParams(e)
+        }}
       >
         <div className='relative w-fit z-10'>
           <ListboxButton className="custom-filter__btn">
@@ -40,7 +51,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                   className={({ focus }) =>`relative cursor-default select-none py-2 px-4 ${focus ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                 >
                   {({selected}) => (
-                    <span>
+                    <span className={`block truncate ${ selected ? 'font-medium': 'font-normal'}`} >
                       {option.title}
                     </span>
                   )}
